60822dcd589ece04f5aad932d764bb108cefa042
Bryan Thai <bthai@pleasantonusd.net>
2024-08-27 00:55:16 -0700

    initial

---
+++ Main.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.event.*;
+import kareltherobot.*;
+//This is the starter code for Karel the Robot AP CSA Thai
+
+public class Main implements Directions {
+  public static void main(String[] args) 
+	{
+		//My robot's name is Fred
+    Robot fred = new Robot(5,1,East,9);
+    World.setVisible(true);
+    World.setSize(10,10);
+    World.setDelay(100);
+
+		fred.move();
+		fred.putBeeper();
+		fred.move();
+		fred.putBeeper();
+		fred.move();
+		fred.turnLeft();
+    fred.move();
+    fred.putBeeper();
+    turnRight(fred);
+
+  }
+    public static void turnRight(Robot placeHolder){
+      placeHolder.turnLeft();
+      placeHolder.turnLeft();
+      placeHolder.turnLeft();
+
+    }
+  
+}
